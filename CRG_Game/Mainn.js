70e08e3eafd9e5bd4d4007a4de63a6e94be69809
Manel <44789216+mduvens@users.noreply.github.com>
2019-12-14 21:07:20 +0000

    v2

--- CRG_Game/Mainn.js
+++ CRG_Game/Mainn.js
@@ -1,8 +1,7 @@
 Physijs.scripts.worker = 'physijs_worker.js';
 Physijs.scripts.ammo = 'ammo.js';
-// raycaster = new THREE.Raycaster( new THREE.Vector3(), new THREE.Vector3( 0, - 1, 0 ), 0, 10 );
-// onObject = false;
 
+var posBig = new THREE.Vector3(-3900, 100, 3000)
 //world
 var renderer = new THREE.WebGLRenderer({
     antialias: true
@@ -10,7 +9,7 @@ var renderer = new THREE.WebGLRenderer({
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 var scene = new Physijs.Scene();
-scene.setGravity(new THREE.Vector3(0, 0, 0))
+scene.setGravity(new THREE.Vector3(0, -1110, 0))
 var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 10, 20000);
 var clock = new THREE.Clock({
     autoStart: true
@@ -20,15 +19,17 @@ controls = new THREE.PointerLockControls(camera, renderer.domElement)
 //************************************************************* */
 // VARIÁVEIS
 var
+    red = 0xFF0000,
+    green = 0x00FF00,
+    blue = 0x0000FF,
     listener = new THREE.AudioListener(),
     sound = new THREE.PositionalAudio(listener),
     sound1 = new THREE.Audio(listener),
     audioLoader = new THREE.AudioLoader(),
     audioLoader1 = new THREE.AudioLoader(),
     light = new THREE.AmbientLight(0xFFFFFF, 1),
-    plight = new THREE.PointLight( 0xffffff, 2, 300 ),
-
-
+    plight = new THREE.PointLight(0xffffff, 2, 300),
+    
     backG = [
         'textures/bkg1_right.png', 'textures/bkg1_left.png',
         'textures/bkg1_top.png', 'textures/bkg1_bot.png',
@@ -64,14 +65,18 @@ var
     geometriaLimite = new THREE.PlaneGeometry(8000, 1000),
     paredesLimite = [],
     materialInterior = Physijs.createMaterial(new THREE.MeshBasicMaterial({
-        color: 0x808080,
+        color: 0x303030,
         side: THREE.DoubleSide,
         map: new THREE.TextureLoader().load('textures/blue.png')
     })),
     geometriaInterior = new THREE.BoxGeometry(7000, 500, 50),
-    red = 0xFF0000,
-    green = 0x00FF00,
-    blue = 0x0000FF,
+    textureDanger = new THREE.TextureLoader().load('textures/dangerZ.jpg'),
+    gemetriaMeshZona = new THREE.BoxGeometry(30, 200, 200),
+    materialMeshZona = new THREE.MeshBasicMaterial({
+        color: green,
+        map: textureDanger
+    }),
+    textureFire = new THREE.TextureLoader().load('textures/fire.jpg'),
     z = 0,
     s = 0,
     i = 0,
@@ -107,18 +112,22 @@ var
     model,
     loader = new THREE.GLTFLoader(),
     zombieASeguir = false,
-    numeroZombies = 5,
+    numeroZombies = 10,
     zombies = [],
-    numeroComida = 3,
+    numeroComida = 1,
     comidas = [],
     rPressionado = false,
     cont = 0,
-    offset = 0.01,
+    offset = 0.05,
     add = false,
     sprint = false,
-    velocidadeJogador
+    velocidadeJogador,
+    meshesZonas = []
 
-
+for (let j = 0; j < 4; j += 1) {
+    meshZona = new THREE.Mesh(gemetriaMeshZona, materialMeshZona)
+    meshesZonas.push(meshZona)
+}
 for (let j = 0; j < 4; j += 1) {
     var paredeL = new Physijs.BoxMesh(geometriaLimite, materialLimite);
     paredesLimite.push(paredeL)
@@ -127,6 +136,10 @@ for (let i = 0; i < 3; i += 1) {
     var paredeI = new Physijs.BoxMesh(geometriaInterior, materialInterior, 0)
     paredesInterior.push(paredeI)
 }
+meshesZonas[0].position.set(-3900, 100, 3000)
+meshesZonas[1].position.set(3900, 100, 1000)
+meshesZonas[2].position.set(-3900, 100, -1000)
+meshesZonas[3].position.set(3900, 100, -3000)
 paredesLimite[0].position.set(4000, 500, 0)
 paredesLimite[0].rotation.y += Math.PI / 2
 paredesLimite[1].position.set(-4000, 500, 0)
@@ -140,52 +153,34 @@ mira1.position.z -= 500
 mira2.position.z -= 500
 chao.rotation.x = Math.PI / 2
 chao.name = 'chao'
-teto.position.set(0,1000,0)
-teto.rotation.x += Math.PI/2
+teto.position.set(0, 1000, 0)
+teto.rotation.x += Math.PI / 2
 //teto.rotation.z += Math.PI/2
 plight.position.z -= 50
 
 // teste colisao paredes
-for(let i=0; i<paredesInterior.length; i+=1){
-    paredesInterior[i].setCcdMotionThreshold(200);
-    paredesInterior[i].setCcdSweptSphereRadius(0.1);
-    paredesInterior[i].name = 'paredeI'
-    paredesInterior[i].addEventListener('collision', function(obj, vel, ang){
-            if(obj.name == 'zombie'){
-                obj.junto = true
-                console.log(obj)
-                bang = true
-                velBang.z = vel.z   
-                console.log(velBang)      
-            }
-        
-    })
-}
-
+// for (let i = 0; i < paredesInterior.length; i += 1) {
+//     paredesInterior[i].setCcdMotionThreshold(2);
+//     paredesInterior[i].setCcdSweptSphereRadius(0.1);
+//     paredesInterior[i].setLinearFactor(new THREE.Vector3(0, 0, 0))
+//     paredesInterior[i].name = 'paredeI'
+// }
 // colisão jogador
 jogador1.mesh.addEventListener('collision', function (obj, vel, angularV) {
-   
-    if (obj.name === 'balaZombie') {
-        if (jogador1.mesh.vida > 20) {
-            audioLoader1.load('sounds/hitSimples.mp3', function (buffer) {
-                sound1.setBuffer(buffer);
-                //sound.setLoop(true);
-                sound1.setVolume(0.9);
-                sound1.play();
-            })
-        }
+    if (obj.name === 'balaBig' || obj.name == 'zombie') {
         if (jogador1.mesh.vida > 0) {
-            jogador1.mesh.vida -= 50;
+            jogador1.mesh.vida -= 100;
             //console.log(obj)
         }
     }
     if (obj.name === 'comida') {
-        audioLoader1.load('sounds/coin.wav', function (buffer) {
-            sound1.setBuffer(buffer);
-            //sound.setLoop(true);
-            sound1.setVolume(0.9);
-            sound1.play();
-        })
+        // audioLoader1.load('sounds/coin.wav', function (buffer) {
+        //     sound1.setBuffer(buffer);
+        //     //sound.setLoop(true);
+        //     sound1.setVolume(0.9);
+        //     sound1.play();
+        // })
+        sound1.play();
         obj.alive = false;
         scene.remove(obj)
         pontos++;
@@ -194,7 +189,7 @@ jogador1.mesh.addEventListener('collision', function (obj, vel, angularV) {
     //console.log(jogador1.mesh.vida)
 })
 
-let loaded = false;
+let loaded = false
 loader.load('obj/scene.gltf', function (gltf) {
     model = gltf.scene
     model.scale.set(.15, .15, .15)
@@ -220,10 +215,10 @@ document.addEventListener('keyup', onKeyUp, false);
 
 // gun.mesh.rotation.z += Math.PI/2
 
-
+let loadedInit = false
 function init() {
     camera.position.set(3400, 100, 3200)
-   // camera.rotation.y += Math.PI/2
+    camera.rotation.set(0,Math.PI/2,0)
     scene.add(camera)
     scene.background = bLoader.load(backG);
     camera.add(listener)
@@ -238,159 +233,87 @@ function init() {
     camera.add(mira2)
     addZombies()
     addComida()
+    audioLoader1.load('sounds/coin.wav', function (buffer) {
+        for(let j = 0; j<comidas.length;j+=1){
+            sound1.setBuffer(buffer);
+            sound1.setVolume(0.9);
+            comidas[j].mesh.add(sound1)
+        }
+        })
+
     scene.add(controls.getObject());
+    for (i = 0; i < meshesZonas.length; i += 1) {
+        scene.add(meshesZonas[i])
+    }
     for (i = 0; i < paredesLimite.length; i += 1) {
         scene.add(paredesLimite[i])
     }
     for (i = 0; i < paredesInterior.length; i += 1) {
         scene.add(paredesInterior[i])
     }
-
+    loadedInit = true
     renderer.render(scene, camera)
 
 }
+balasBig = []
+let g = 0,
+    dir, dir1q,
+    naZona = true
 
 function animate() {
-    //controls.update();
-    //camera.lookAt(cube)
-    // raycaster.ray.origin.copy( controls.getObject().position );
-    // raycaster.ray.origin.y -= 20;
-    // var intersections = raycaster.intersectObjects(cubosTest);
-    // onObject = intersections.length > 0  
+    delta = clock.getDelta()
+    jogador1.mesh.__dirtyPosition = true
+    movimentoInimigos()
+    limitarMovJogador();
+    naZona = verificarZona()
+
     if (controls.isLocked === true) {
-        if(add == false){
-            camera.add(model)
-            add = true
-        }
-        disparoAutomatico()
-        //teste colisao paredes
-        jogador1.mesh.__dirtyPosition = true
-        if(bang == true){
-            //camera.position.x -= velBang.x /100
-            if(velBang.z > 0){
-                //camera.position.z -= 150
-                bang = false  
-            }
-            else if (velBang.z < 0){
-                //camera.position.z += 150
-                bang = false  
-            }
-         }
-        // gun.mesh.__dirtyPosition = true
-        // gun.mesh.__dirtyRotation = true
         for (let m = 0; m < numeroComida; m += 1) {
             comidas[m].mesh.__dirtyRotation = true;
             comidas[m].mesh.rotation.z += offset
             comidas[m].mesh.rotation.x += offset
         }
-        jogador1.mesh.position.set(camera.position.x, camera.position.y, camera.position.z)
+        if (add == false) {
+            camera.add(model)
+            add = true
+        }
         
+        disparoAutomatico()
+        jogador1.mesh.position.set(camera.position.x, camera.position.y, camera.position.z)
         frame += 1;
-        for (let m = 0; m < numeroComida; m += 1) {
-            if (comidas[m].mesh.alive) {
-                comidas[m].mesh.setLinearFactor(new THREE.Vector3(0, 0, 0))
-            }
-        }
-        if (frame % 60 == 0) {
-            console.log(noObjeto)
-        }
-        delta = clock.getDelta()
-        //console.log(delta)
 
-        limitarMovJogador();
+        // for (let m = 0; m < numeroComida; m += 1) {
+        //     if (comidas[m].mesh.alive) {
+        //         comidas[m].mesh.setLinearFactor(new THREE.Vector3(0, 0, 0))
+        //     }
+        // }
+        // Movimento Jogador
         velocityJ.x -= velocityJ.x * 10.0 * delta;
         velocityJ.z -= velocityJ.z * 10.0 * delta;
-        if (noObjeto == true) {
-            velocityJ.y = 0
-            podeSaltar = true
-        } else {
-            velocityJ.y -= 9.8 * 500.0 * delta;
-
-        }
-
-        // Movimento Jogador
+        velocityJ.y -= 9.8 * 500.0 * delta;
         controls.moveRight(velocityJ.x * delta);
         controls.moveForward(velocityJ.z * delta);
         controls.getObject().position.y += (velocityJ.y * delta); // new behavior
-
         if (controls.getObject().position.y < 150) {
             velocityJ.y = 0;
             controls.getObject().position.y = 150;
             podeSaltar = true;
         }
-        // zombies a seguir
-        for (n = 0; n < zombies.length; n += 1) {
-            if (zombies[n].mesh.alive) {
-                zombies[n].mesh.__dirtyPosition = true;
-                zombies[n].mesh.setCcdMotionThreshold(40);
-                zombies[n].mesh.setCcdSweptSphereRadius(0.2);
-                distanciaJ = new THREE.Vector3(controls.getObject().position.x - zombies[n].mesh.position.x, controls.getObject().position.y - zombies[n].mesh.position.y, controls.getObject().position.z - zombies[n].mesh.position.z)
-                direcao = obterDirecao(zombies[n].mesh, camera)
-                for(i=0;i<paredesInterior.length;i +=1){
-                    distanciaP = new THREE.Vector3(controls.getObject().position.x - zombies[n].mesh.position.x, controls.getObject().position.y - zombies[n].mesh.position.y, controls.getObject().position.z - zombies[n].mesh.position.z)
-                }
-                if (distanciaJ.x < 2000 && distanciaJ.z < 2000) {
-                    zombies[n].aSeguir(true);
-                    if (zombies[n].mesh.junto == false) {
-                        zombies[n].mesh.position.x += direcao.x * delta / 2
-                        zombies[n].mesh.position.z += direcao.z * delta / 2
-                        zombies[n].mesh.setLinearVelocity(new THREE.Vector3(direcao.x * 100, 0, direcao.z * 100))
-                    } else {
-                        zombies[n].mesh.position.x -= direcao.x * delta * 5
-                        zombies[n].mesh.position.z -= direcao.z * delta * 5
-                        zombies[n].mesh.junto = false
-                    }
-                } else {
-                    zombies[n].aSeguir(false);
-                }
-            }
-        }
-        // Disparo zombie
-        if (frame % 180 === 0) {
-            // console.log(camera.position)
-            //console.log(camera.position)   
-            if (balasZ.length === zombies.length) {
-                for (c = 0; c < balasZ.length; c += 1) {
-                    //if (balasZ[c].mesh.alive)
-                    //scene.remove(balasZ[c].mesh) // error a undefined
-                }
-            }
-            for (c = 0; c < balasZ.length; c += 1) {
-                //scene.remove(balasZ[c])
-            }
-            for (c = 0; c < zombies.length; c += 1) {
-                if (zombies[c].mesh.alive) {
-                    balasZ[c] = new balaZ();
-                    balasZ[c].mesh.alive = true;
-                }
-            }
-            for (i = 0; i < balasZ.length; i += 1) {
-                if (zombies[i].mesh.alive === true) {
-                    distance = new THREE.Vector3(controls.getObject().position.x - zombies[i].mesh.position.x, controls.getObject().position.y - zombies[i].mesh.position.y, controls.getObject().position.z - zombies[i].mesh.position.z)
-                    // balasZ[i].mesh.addEventListener('collision', function (obj, lV, aV) {
-                    //     console.log(obj.name)
-                    // })
-                    balasZ[i].mesh.position.set(zombies[i].mesh.position.x, zombies[i].mesh.position.y, zombies[i].mesh.position.z)
-                    direcoesB = obterDirecao(zombies[i].mesh, controls.getObject())
-                    if(zombies[i].mesh.rage){
-                        scene.add(balasZ[i].mesh)
-                    }
-                    balasZ[i].mesh.__dirtyPosition = true
-                    balasZ[i].mesh.setLinearFactor(new THREE.Vector3(0, 0, 0))
-                    balasZ[i].mesh.setLinearVelocity(new THREE.Vector3(direcoesB.x, direcoesB.y, direcoesB.z))
-                }
-            }
-        }
         if (jogador1.mesh.vida === 0) {
             alert("MORRESTE")
             limparMapa()
-            init()
+            pontos = 0
+            deadZombies = 0
+            addZombies()
+            addComida()
             camera.position.set(3400, 100, 3200)
             jogador1.mesh.vida = 100;
+            console.log(numeroZombies)
+            camera.rotation.set(0,Math.PI/2,0)
+    
         }
         if (frame % 60 === 0) {
-           // console.log(camera.rotation)
-            if (deadZombies === numeroZombies || pontos === numeroComida) {
+            if (pontos === numeroComida) {
                 velocityJ.x = 0;
                 velocityJ.z = 0;
                 velocityJ.y = 0;
@@ -399,14 +322,18 @@ function animate() {
                 alert("PREPARA-TE. RONDA " + (numeroZombies / 5 + 1));
                 novaRonda()
                 camera.position.set(3400, 100, 3200)
+                camera.rotation.set(0,Math.PI/2,0)
             }
         }
+        dispararBig()
     }
-    scene.simulate();
     ajustarJanela();
+    scene.simulate();
     renderer.render(scene, camera)
     requestAnimationFrame(animate);
 
 }
 init()
-animate()
+if(loadedInit){
+    animate()
+}
